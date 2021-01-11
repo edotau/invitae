fdbc09b8cd4f98e1375f3df97d0476cc3437f20c
Eric Au <ericdotau@gmail.com>
2021-01-10 23:38:26 -0800

    additional annotation and updated usage message

--- covid_sequencing/covid_sequencing_assembly.sh
+++ covid_sequencing/covid_sequencing_assembly.sh
@@ -1,32 +1,45 @@
 #!/bin/sh
-export PATH=/Users/eric.au/bin/SPAdes-3.14.1-Darwin/bin:$PATH
-
 REF=wuhCor1/wuhCor1.fa
+
 READ1=$1
 READ2=$2
 
-if [[ "$#" -lt 2 ]]
-then
-	echo "Usage:
+if [[ "$#" -lt 2 ]]; then
+	echo "
+Generate covid draft assemblies from raw fastq data
+Usage:
 	./$(basename "$0") READ1.fastq.gz READ2.fastq.gz
-"
+	"
 	exit 0
-	fi
+fi
 
 PREFIX=$(basename $READ1  | rev | cut -d '.' -f 3- | rev | cut -d '_' -f 1)
 CORES=$(sysctl -n hw.ncpu)
+
+# Assemble initial contigs with SPAdes 3.14.1
+export PATH=/Users/eric.au/bin/SPAdes-3.14.1-Darwin/bin:$PATH
+# Paper:	https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3342519/pdf/cmb.2012.0021.pdf
+# Github:	https://github.com/ablab/spades
+# Manual	https://cab.spbu.ru/software/spades
 
 echo "
 spades.py --only-assembler -o ${PREFIX}_covid_spades -t $CORES --rna -1 $READ1 -2 $READ2"
 spades.py --only-assembler -o ${PREFIX}_covid_spades -t $CORES --rna -1 $READ1 -2 $READ2
 
 DRAFT_FASTA=${PREFIX}_covid_spades/transcripts.fasta
+
+# Perform reference guided assembly:
+# Paper:	https://genomebiology.biomedcentral.com/track/pdf/10.1186/s13059-019-1829-6.pdf
+# Github:	https://github.com/malonge/RagTag
 export PATH=/Users/eric.au/miniconda3/bin:$PATH
+# Nucmer computed Genome to Genome alignments
+# http://mummer.sourceforge.net/
 
 echo "
 ragtag.py scaffold --aligner nucmer -o ${PREFIX}.covid.guided_assembly $REF $DRAFT_FASTA"
 ragtag.py scaffold --aligner nucmer -o ${PREFIX}.covid.guided_assembly $REF $DRAFT_FASTA
 
+# Simple filter for scaffolds and removed very fragmented contigs
 echo "
 faFilter -name=*NC_045512v2* ${PREFIX}.covid.guided_assembly/ragtag.scaffolds.fasta ${PREFIX}.covid.guided_assembly.FINAL.fa"
 faFilter -name=*NC_045512v2* ${PREFIX}.covid.guided_assembly/ragtag.scaffolds.fasta ${PREFIX}.covid.guided_assembly.FINAL.fa
@@ -34,15 +47,17 @@ faFilter -name=*NC_045512v2* ${PREFIX}.covid.guided_assembly/ragtag.scaffolds.fa
 touch ${PREFIX}_stats.txt
 rm ${PREFIX}_stats.txt
 
+# Calculating genome size
 echo "
 faSize -detailed ${PREFIX}.covid.guided_assembly.FINAL.fa >> ${PREFIX}_stats.txt"
 faSize -detailed ${PREFIX}.covid.guided_assembly.FINAL.fa >> ${PREFIX}_stats.txt
 
 export PATH=/Users/eric.au/src/github.com/vertgenlab/gonomics/cmd/faGaps:$PATH
 
+# Estimated gap regions output in BED format
 echo "
-faGap -bed ${PREFIX}.covid.guided_assembly.FINAL.fa ${PREFIX}.covid.guided_assembly.FINAL.GAPS.bed"
-faGap -bed ${PREFIX}.covid.guided_assembly.FINAL.fa ${PREFIX}.covid.guided_assembly.FINAL.GAPS.bed
+faGaps -bed ${PREFIX}.covid.guided_assembly.FINAL.fa ${PREFIX}.covid.guided_assembly.FINAL.GAPS.bed"
+faGaps -bed ${PREFIX}.covid.guided_assembly.FINAL.fa ${PREFIX}.covid.guided_assembly.FINAL.GAPS.bed
 
 GAPS=$(wc -l ${PREFIX}.covid.guided_assembly.FINAL.GAPS.bed)
 echo "Number of Gaps: $GAPS" >> ${PREFIX}_stats.txt
